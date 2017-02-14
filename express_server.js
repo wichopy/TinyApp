d8b31eae77cfe088ec45eb8da7833aa44cb9499d
William Chou <w.chou06@gmail.com>
2017-02-14 17:34:54 +0000

    got displaying short urls and single page short/long url page owrking

---
+++ express_server.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+var morgan = require("morgan");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ entended: true }));
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/urls", (req, res) => {
+  var templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // debug statement to see POST parameters
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
